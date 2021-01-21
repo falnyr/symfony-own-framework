d8e96d163578aad9a41b6334dd931f0d941d0d89
Jan Alfred Richter <falnyr@gmail.com>
2021-01-22 06:58:48 +1300

    feat: simple templating

--- src/app.php
+++ src/app.php
@@ -1,10 +1,25 @@
 <?php
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
 
-$routes = new RouteCollection();
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = date('Y');
+    }
 
-$routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
-$routes->add('bye', new Route('/bye'));
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
+$routes = new Routing\RouteCollection();
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => function ($request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year!');
+        }
+
+        return new Response('Nope, this is not a leap year.');
+    }
+]));
 
 return $routes;
